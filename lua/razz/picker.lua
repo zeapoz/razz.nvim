4f5946c57d79796a3ecc59fef30c983b239e7989
zeapoz <zeapo@pm.me>
2026-04-23 17:28:39 +0900

    feat: open multiple notes

--- lua/razz/picker.lua
+++ lua/razz/picker.lua
@@ -79,10 +79,13 @@ function M.show_notes(opts)
   if on_select then
     picker_opts.attach_mappings = function(prompt_bufnr)
       actions.select_default:replace(function()
+        local picker = action_state.get_current_picker(prompt_bufnr)
+        local multiselect = picker:get_multi_selection()
         local selection = action_state.get_selected_entry()
         if selection then
           actions.close(prompt_bufnr)
-          on_select(selection.value)
+          local selections = #multiselect > 0 and multiselect or { selection }
+          on_select(selection, selections)
         end
       end)
       return true
@@ -90,6 +93,82 @@ function M.show_notes(opts)
   end
 
   pickers.new({}, picker_opts):find()
+end
+
+local function open_note_buffer(note, game_id, prev_winnr, focus)
+  local note_addr = note.Address
+  local buf = vim.api.nvim_create_buf(true, false)
+  vim.bo[buf].modifiable = true
+  vim.bo[buf].fileformat = "dos"
+  local note_content = helpers._unescape_content(note.Note)
+  local lines = vim.split(note_content, "\r\n", { keepempty = true })
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+  vim.bo[buf].modified = false
+
+  local normalized = helpers._normalize_for_display(note.Note)
+  local first_line = normalized:match("^[^\n]*")
+  vim.api.nvim_buf_set_name(buf, note_addr .. ": " .. first_line)
+  vim.bo[buf].filetype = "text"
+
+  local function to_export_content(buf_lines)
+    local content = table.concat(buf_lines, "\r\n")
+    content = helpers._escape_content(content)
+    return content
+  end
+
+  if focus ~= false then
+    vim.api.nvim_win_set_buf(0, buf)
+  end
+
+  vim.api.nvim_create_autocmd("BufWriteCmd", {
+    buffer = buf,
+    callback = function()
+      local current_lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+      local current_content = to_export_content(current_lines)
+
+      local normalized_content = helpers._unescape_content(current_content)
+
+      local razz = require("razz")
+      local server_notes = razz.load_server_notes(game_id)
+      local server_note = helpers.find_note_by_addr(server_notes, note_addr)
+      local server_note_content = server_note and server_note.Note or nil
+
+      local export_note = { Address = note_addr, Note = current_content }
+
+      if server_note_content and normalized_content == server_note_content then
+        local ok, err = razz.delete_local_note(game_id, note_addr)
+        if ok then
+          vim.notify("Note matches server, removed local: " .. note_addr)
+        else
+          vim.notify("Failed to remove local: " .. err, vim.log.levels.ERROR)
+        end
+      else
+        local ok, err = razz.export_note(game_id, export_note)
+        if ok then
+          if server_note_content then
+            vim.notify("Updated note: " .. note_addr)
+          else
+            vim.notify("Added new note: " .. note_addr)
+          end
+        else
+          vim.notify("Export failed: " .. err, vim.log.levels.ERROR)
+        end
+      end
+
+      vim.bo[buf].modified = false
+    end,
+  })
+
+  vim.bo[buf].buftype = "acwrite"
+
+  vim.api.nvim_create_autocmd("BufUnload", {
+    buffer = buf,
+    callback = function()
+      vim.api.nvim_set_current_win(prev_winnr)
+    end,
+  })
+
+  return buf
 end
 
 function M.open_note(opts)
@@ -106,74 +185,16 @@ function M.open_note(opts)
 
   opts.notes = notes
 
-  opts.on_select = function(note)
+  opts.on_select = function(selection, selections)
     local prev_winnr = vim.api.nvim_get_current_win()
-    local note_addr = note.Address
-    local buf = vim.api.nvim_create_buf(true, false)
-    vim.bo[buf].fileformat = "dos"
-    local note_content = helpers._unescape_content(note.Note)
-    local lines = vim.split(note_content, "\r\n", { keepempty = true })
-    vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
-    vim.api.nvim_buf_set_name(buf, "Note: " .. note_addr)
-    vim.bo[buf].filetype = "text"
-    vim.bo[buf].modifiable = true
 
-    local function to_export_content(buf_lines)
-      local content = table.concat(buf_lines, "\r\n")
-      content = helpers._escape_content(content)
-      return content
+    local selection_value = selection.value
+    for _, entry in ipairs(selections) do
+      if entry.value ~= selection_value then
+        open_note_buffer(entry.value, game_id, prev_winnr, false)
+      end
     end
-
-    local original_content = to_export_content(lines)
-
-    vim.cmd("buffer " .. buf)
-
-    vim.api.nvim_create_autocmd("BufWriteCmd", {
-      buffer = buf,
-      callback = function()
-        local current_lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
-        local current_content = to_export_content(current_lines)
-
-        local normalized_content = helpers._unescape_content(current_content)
-
-        local server_notes = razz.load_server_notes(game_id)
-        local server_note = helpers.find_note_by_addr(server_notes, note_addr)
-        local server_note_content = server_note and server_note.Note or nil
-
-        local export_note = { Address = note_addr, Note = current_content }
-
-        if server_note_content and normalized_content == server_note_content then
-          local ok, err = razz.delete_local_note(game_id, note_addr)
-          if ok then
-            vim.notify("Note matches server, removed local: " .. note_addr)
-          else
-            vim.notify("Failed to remove local: " .. err, vim.log.levels.ERROR)
-          end
-        else
-          local ok, err = razz.export_note(game_id, export_note)
-          if ok then
-            if server_note_content then
-              vim.notify("Updated note: " .. note_addr)
-            else
-              vim.notify("Added new note: " .. note_addr)
-            end
-          else
-            vim.notify("Export failed: " .. err, vim.log.levels.ERROR)
-          end
-        end
-
-        vim.bo[buf].modified = false
-      end,
-    })
-
-    vim.bo[buf].buftype = "acwrite"
-
-    vim.api.nvim_create_autocmd("BufUnload", {
-      buffer = buf,
-      callback = function()
-        vim.api.nvim_set_current_win(prev_winnr)
-      end,
-    })
+    open_note_buffer(selection_value, game_id, prev_winnr, true)
   end
 
   M.show_notes(opts)
